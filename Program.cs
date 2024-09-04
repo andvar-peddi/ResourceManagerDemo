06f07601defb4683f41787b2353feca7e0dc9b7a
AndreiAmbo <andrei.varga@ambo.ro>
2024-09-04 16:15:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace ResourceManagerDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ResourceManager rm = new ResourceManager("ResourceManagerDemo.Resources.Strings", Assembly.GetExecutingAssembly());
+
+            Console.WriteLine(rm.GetString("Welcome", new CultureInfo("EN")));
+            Console.WriteLine(rm.GetString("Welcome", new CultureInfo("IT")));
+            Console.WriteLine(rm.GetString("Welcome", new CultureInfo("RO")));
+        }
+    }
+}
